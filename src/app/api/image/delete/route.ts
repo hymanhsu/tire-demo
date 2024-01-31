484d35e778efd338b971c1e4f136e2af5528cd79
Hyman.Hsu <hymanhsu8175@gmail.com>
2024-01-30 22:12:30 -0800

    add upload file component

--- src/app/api/image/delete/route.ts
+++ src/app/api/image/delete/route.ts
@@ -1,25 +1,17 @@
 import { del } from '@vercel/blob';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 /**
  * /api/image/delete?url=
  * @param request 
  * @returns 
  */
-export async function GET(request: NextRequest): Promise<NextResponse> {
-    try {
-        const urlToDelete = request.nextUrl.searchParams.get("url") as string;
-        await del(urlToDelete);
-
-        return Promise.resolve(NextResponse.json({
-            status: "ok",
-        }));
-    } catch (error) {
-        return Promise.resolve(NextResponse.json(
-            {
-                status: "error",
-                message: (error as Error).message
-            }
-        ));
-    }
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const urlToDelete = searchParams.get('url') as string;
+    await del(urlToDelete);
+   
+    return new Response();
 }
